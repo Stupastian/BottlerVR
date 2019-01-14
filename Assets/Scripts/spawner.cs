5afbdce283e06220823bc16b8feb1ba4cb77f15b
STU\Mikko <mikkoruot@gmail.com>
2019-01-14 22:25:44 +0200

    Added bottle spawner Added Skeleton for Destroyer

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public Transform spawnerLocation;
+    public GameObject bottle;
+    
+    [SerializeField] float timer = 1f;
+    [SerializeField] float startTime = 3f;
+    bool running = true;
+    private void Start()
+    {
+        InvokeRepeating("SpawnBottle", startTime, timer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnBottle()
+    {
+        Instantiate(bottle, spawnerLocation.position, spawnerLocation.rotation);
+        
+    }
+}
