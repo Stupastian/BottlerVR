67c497551b005585733c42c87d3f44284eeb00c5
STU\Mikko <mikkoruot@gmail.com>
2019-01-16 00:34:43 +0200

    Added Cap creator script skeleton

---
+++ Assets/Scripts/CapCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapCreator : MonoBehaviour
+{
+
+    public GameObject bottleCap;
+    public Vector3 capPosition;
+    public Quaternion capRotation;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "bottle")
+        {
+            //TODO create a bottle cap and attach it to the bottle that collided
+            //Instantiate(bottleCap, collision.gameObject.transform);
+            //bottleCap.transform.parent = collision.gameObject.transform;
+            //bottleCap.transform.localPosition = bottleCap.transform.parent.localPosition;
+            //bottleCap.transform.localEulerAngles = bottleCap.transform.parent.localRotation;
+        }
+    }
+
+}
