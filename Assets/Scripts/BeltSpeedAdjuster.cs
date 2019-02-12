10e7b71f1d8d4ec6e4fda468b45681776ecf17cd
STU\Mikko <mikkoruot@gmail.com>
2019-02-12 20:06:02 +0200

    Added script to adjust belt speed with the lever

---
+++ Assets/Scripts/BeltSpeedAdjuster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltSpeedAdjuster : MonoBehaviour
+{
+
+    public GameObject speedController;
+    public GameObject adjustmentTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(speedController.transform.localRotation > speedController.GetComponent<HingeJoint>().limits.min)
+        adjustmentTarget.GetComponent<ConveyorBelt>().speed += .5f;
+        Debug.Log("Speed increased!");
+    }
+
+}
