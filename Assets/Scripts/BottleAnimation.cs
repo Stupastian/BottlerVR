39ac680f73b0d0195a6160e01b137a7192529460
STU\Mikko <mikkoruot@gmail.com>
2019-02-23 16:58:13 +0200

    Added scripts for bottle animation

---
+++ Assets/Scripts/BottleAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleAnimation : MonoBehaviour
+{
+    Animator bottleAnimator;
+    float triggerDistance;
+    public GameObject player;
+    public GameObject target;
+    public float triggerActivationDistance;
+    public float triggerDeActiovationDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottleAnimator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        triggerDistance = Vector3.Distance(player.transform.position, target.transform.position);
+        Debug.Log("trggerDistance = " +triggerDistance);
+        if (triggerDistance < triggerActivationDistance)
+        {
+            bottleAnimator.SetBool("PlayerTooClose", true);
+        }
+        if (triggerDistance > triggerDeActiovationDistance)
+        {
+            bottleAnimator.SetBool("PlayerTooClose", true);
+        }
+
+    }
+}
+
+
